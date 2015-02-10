4ad5fca3c45291c704c825ff6a5cb1c5802ac4ba
Stephen Prill <stephen.prill@gmail.com>
2015-02-10 12:38:18 -0700

    authorized instagram with app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :users
   root "pages#index"
-  get "/auth/:provider/callback" => "users#instragram"
+  get "/auth/:provider/callback" => "authentications#create"
 end
